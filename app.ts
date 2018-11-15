04c80b53940114673d03da15fb8b4e975e557644
Netter <rafal.netter@aecom.com>
2018-11-15 11:59:01 +0100

    Change app.ts for testing tsc compilation

--- app.ts
+++ app.ts
@@ -1,2 +1,3 @@
-var todo: string = "Pick up drycleaning";
-var todo2: string = "Clean Batcave";
+let todo: string = "Pick up drycleaning";
+let todo2: string = "Clean Batcave";
+let todo3: string = "Clean Batcave NEW FILE";
